ca26be0b7ae6842ccdd6bed9492b0754c7929b12
Piotr Luszczek <luszczek@icl.utk.edu>
2004-07-17 20:55:39 -0400

    Fixed a type with a file name.

--- tools/makefile.py
+++ tools/makefile.py
@@ -183,7 +183,7 @@ def TraverseDirs(prfx, items):
 def Dist(deps, prfx="hpcc"):
     allPrfx = prfx + "/"
 
-    addItems = ["Makefile", "README.xml", "README.html", "README.html", "hpccinf.txt",
+    addItems = ["Makefile", "README.xml", "README.html", "README.txt", "hpccinf.txt",
                 "hpl/Make.UNKNOWN", "hpl/BUGS", "hpl/COPYRIGHT", "hpl/HISTORY",
                 "hpl/HPL.build.log.220120040613", "hpl/INSTALL", "hpl/Make.top", "hpl/Makefile",
                 "hpl/README", "hpl/TODO", "hpl/TUNING", "hpl/lib/arch/build/Makefile.hpcc",
