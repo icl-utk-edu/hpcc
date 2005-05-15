559f8bc6b52617cebeced5e82f529bb7066c724d
Piotr Luszczek <luszczek@icl.utk.edu>
2005-05-15 11:19:22 -0400

    Bumped version to 0.9 Added `candidate' to releases Added micro release number: 0.9.0 The intention is to bump micro numbers and use release (alpha, beta,...) as confidence indicators

--- include/hpccver.h
+++ include/hpccver.h
@@ -1,9 +1,10 @@
 /* -*- mode: C; tab-width: 2; indent-tabs-mode: nil; -*- */
 
 /*
-Version has three components: MAJOR, MINOR and RELEASE
-RELEASE is a, b, f (alpha, beta, and final)
+Version has four components: MAJOR, MINOR, MICRO and RELEASE.
+RELEASE is a, b, c, f (alpha, beta, candidate, and final).
 */
 #define HPCC_VERSION_MAJOR   0
-#define HPCC_VERSION_MINOR   8
-#define HPCC_VERSION_RELEASE 'b'
+#define HPCC_VERSION_MINOR   9
+#define HPCC_VERSION_MICRO   0
+#define HPCC_VERSION_RELEASE 'a'
