21b8e4c5c6e24265fd8a7510036a6d656d442fea
Piotr Luszczek <luszczek@icl.utk.edu>
2005-07-08 11:30:58 -0400

    Working MPI RandomAccess

--- python/hpcc.py
+++ python/hpcc.py
@@ -4,8 +4,8 @@
 # HPC Challenge Benchmark
 # Piotr Luszczek
 # University of Tennessee Knoxville
-# Innovative Computing Laboratories                                 
-# (C) Copyright 2005 All Rights Reserved                       
+# Innovative Computing Laboratories
+# (C) Copyright 2005 All Rights Reserved
 #
 
 import sys
@@ -15,11 +15,82 @@ import numarray.ufunc as U
 
 import mpi
 
-SIZEOF_INT = 4
-SIZEOF_DOUBLE = 8
-SIZEOF_UINT64 = 8
+G_SIZEOF_INT = 4
+G_SIZEOF_DOUBLE = 8
+G_SIZEOF_UINT64 = 8
 
-LONG_IS_64BITS = False
+G_LONG_IS_64BITS = False
+
+G_MAX_TOTAL_PENDING_UPDATES = 1024
+G_LOCAL_BUFFER_SIZE = G_MAX_TOTAL_PENDING_UPDATES
+G_USE_MULTIPLE_RECV = True or  False
+G_MAX_RECV = G_USE_MULTIPLE_RECV and 16 or 1
+
+G_INT64_DT = mpi.data_type(N.array(0, type=N.UInt64))
+
+G_FINISHED_TAG = 1
+G_UPDATE_TAG = 2
+
+G_POLY, G_PERIOD = 7, 1317624576693539401L
+
+def starts(n):
+
+    n = N.array([n], N.Int64)
+    m2 = N.zeros([64], N.UInt64)
+
+    while n[0] < 0:        n += G_PERIOD
+    while n[0] > G_PERIOD: n -= G_PERIOD
+    if n[0] == 0: return 1
+
+    temp = N.array([1], N.UInt64)
+    ival = N.array([0], N.UInt64)
+    for i in range(64):
+        m2[i] = temp[0]
+        temp = (temp << 1) ^ ((temp.astype(N.Int64)<0)[0] and G_POLY or 0)
+        temp = (temp << 1) ^ ((temp.astype(N.Int64)<0)[0] and G_POLY or 0)
+    for i in range(62, -1, -1):
+        if ((n>>i) & 1)[0]: break
+
+    ran = N.array([2], N.UInt64)
+    while (i > 0):
+        temp[0] = 0
+        for j in range(64):
+            if ((ran>>j) & 1)[0]:
+                temp ^= m2[j:j+1]
+        ran[0] = temp[0]
+        i -= 1
+        if ((n>>i) & 1)[0]:
+            ran = (ran << 1) ^ ((ran.astype(N.Int64)<0)[0] and G_POLY or 0)
+    return ran[0]
+
+class Buckets:
+    def __init__(self, n_keys, n_vals):
+        self.d = {}
+
+    def InsertUpdate(self, v, key):
+        d = self.d
+        if not d.has_key(key):
+            d[key] = [v]
+        else:
+            d[key].append(v)
+
+    def GetUpdates(self, buf): # get an entry with
+        d = self.d
+
+        lmax = 0
+        for k in d:
+            l = len(d[k])
+            if l > lmax:
+                lmax = l
+                key = k
+
+        #print lmax, d[key], len(d[key]), buf.type()
+        #buf[:lmax] = d[key]
+        for i in range(lmax):
+            buf[i:i+1] = d[key][i]
+        del d[key]
+
+        return key, lmax
 
 class HPCC:
     def __init__(self, in_f, out_f):
@@ -30,9 +101,9 @@ class HPCC:
         if 0 == self.rank:
             self.log_file = open(out_f, "a")
 
-        self.results = {}
+        self.results = {"mpi_comm_size": self.size}
 
-        if LONG_IS_64BITS:
+        if G_LONG_IS_64BITS:
             self.mpi_int64dt = mpi.LONG
         else:
             self.mpi_int64dt = mpi.LONG_LONG_INT
@@ -48,7 +119,7 @@ class HPCC:
             self.log_file.flush()
 
     def local_vector_size(self, vec_cnt, size, pow2):
-        max_int_bits2 = SIZEOF_INT * 8 - 2
+        max_int_bits2 = G_SIZEOF_INT * 8 - 2
 
         flg2 = 1
         while self.HPL_max_proc_mem / size / vec_cnt >> flg2:
@@ -66,7 +137,7 @@ class HPCC:
 
         times = N.array(type=N.Float64, shape=(4, NTIMES))
 
-        VectorSize = self.local_vector_size( 3, SIZEOF_DOUBLE, False )
+        VectorSize = self.local_vector_size( 3, G_SIZEOF_DOUBLE, False )
 
         scalar = 3.0
 
@@ -99,7 +170,7 @@ class HPCC:
             U.add(b, scalar*c, a)
             times[3, i] += mpi.Wtime()
 
-        return 1e-9 * N.array([2, 2, 3, 3]) * SIZEOF_DOUBLE / N.array(map(min, times[:, 1:])) * VectorSize
+        return 1e-9 * N.array([2, 2, 3, 3]) * G_SIZEOF_DOUBLE / N.array(map(min, times[:, 1:])) * VectorSize
 
     def ep_stream(self):
         lr = self.stream()
@@ -113,10 +184,276 @@ class HPCC:
 
     def mpira_any(self, logTableSize, TableSize, LocalTableSize, MinLocalTableSize, GlobalStartMyProc, Top,
                   Remainder, ProcNumUpdates):
-        pass
+
+        NumProcs, MyProc = self.size, self.rank
+        NumberReceiving = NumProcs - 1
+
+        Table = N.arange(LocalTableSize, type=N.UInt64)
+        Table += GlobalStartMyProc
+
+        if G_USE_MULTIPLE_RECV:
+            inreq = mpi.Request(G_MAX_RECV)
+        else:
+            inreq = mpi.Request()
+        outreq = mpi.Request(); outreq[:] = mpi.REQUEST_NULL
+
+        finish_statuses = mpi.Status(NumProcs)
+        finish_req = mpi.Request(NumProcs)
+
+        status = mpi.Status()
+
+        recv_size = G_LOCAL_BUFFER_SIZE
+        LocalSendBuffer = N.array(type=N.UInt64, shape=recv_size)
+        LocalRecvBuffer = N.array(type=N.UInt64, shape=G_MAX_RECV*recv_size)
+
+        buckets = Buckets(NumProcs, recv_size)
+
+        SendCnt = ProcNumUpdates
+        Ran = N.array([starts(4 * GlobalStartMyProc)], type=N.UInt64)
+
+        pendingUpdates = 0
+        i = 0
+
+        if G_USE_MULTIPLE_RECV:
+            NumRecvs = NumProcs > 4 and min(4,G_MAX_RECV) or 1
+            for j in range(NumRecvs):
+                self.comm.Irecv(LocalRecvBuffer[j*recv_size:(j+1)*recv_size], mpi.ANY_SOURCE, mpi.ANY_TAG, inreq[j])
+        else:
+            self.comm.Irecv(LocalRecvBuffer, mpi.ANY_SOURCE, mpi.ANY_TAG, inreq)
+
+        while i < SendCnt:
+            while True: # receive messages
+                if G_USE_MULTIPLE_RECV:
+                    index, have_done = inreq[:NumRecvs].Testany(status)
+                else:
+                    have_done = inreq.Test(status)
+
+                if have_done:
+
+                    if status.MPI_TAG == G_UPDATE_TAG:
+                        recvUpdates = status.Get_count(G_INT64_DT)
+
+                        if G_USE_MULTIPLE_RECV:
+                            bufferBase = index*recv_size
+                        else:
+                            bufferBase = 0
+
+                        for j in range(recvUpdates):
+                            inmsg = N.array(LocalRecvBuffer[bufferBase+j], type=N.UInt64)
+                            LocalOffset = (inmsg & (TableSize - 1)) - GlobalStartMyProc
+                            Table[LocalOffset] ^= inmsg
+
+                    elif status.MPI_TAG == G_FINISHED_TAG: # we got a done message.  Thanks for playing...
+                        NumberReceiving -= 1
+                    else:
+                        self.comm.Abort()
+
+                    if G_USE_MULTIPLE_RECV:
+                        self.comm.Irecv(LocalRecvBuffer[index*recv_size:(index+1)*recv_size], mpi.ANY_SOURCE, mpi.ANY_TAG, inreq[index])
+                    else:
+                        self.comm.Irecv(LocalRecvBuffer, mpi.ANY_SOURCE, mpi.ANY_TAG, inreq)
+
+                if have_done and NumberReceiving > 0:
+                    continue
+                break
+
+            if pendingUpdates < recv_size:
+                Ran = (Ran << 1) ^ ((Ran.astype(N.Int64) < 0)[0] and G_POLY or 0)
+                GlobalOffset = (Ran & (TableSize-1))[0]
+
+                if GlobalOffset < Top:
+                    WhichPe = GlobalOffset / (MinLocalTableSize + 1)
+                else:
+                    WhichPe = (GlobalOffset - Remainder) / MinLocalTableSize
+
+                if WhichPe == MyProc:
+                    LocalOffset = GlobalOffset - GlobalStartMyProc
+                    Table[LocalOffset] ^= Ran[0]
+                else:
+                    buckets.InsertUpdate(Ran.copy(), WhichPe)
+                    pendingUpdates += 1
+
+                i += 1
+
+            else:
+                have_done = outreq.Test(mpi.STATUS_IGNORE)
+                if have_done:
+                    outreq[:] = mpi.REQUEST_NULL
+                    pe, peUpdates = buckets.GetUpdates(LocalSendBuffer)
+                    self.comm.Isend(LocalSendBuffer[:peUpdates], pe, G_UPDATE_TAG, outreq)
+                    pendingUpdates -= peUpdates
+
+        while pendingUpdates > 0: # send remaining updates in buckets
+
+            while True: # receive messages
+                if G_USE_MULTIPLE_RECV:
+                    index, have_done = inreq[:NumRecvs].Testany(status)
+                else:
+                    have_done = inreq.Test(status)
+
+                if have_done:
+                    if status.MPI_TAG == G_UPDATE_TAG:
+                        recvUpdates = status.Get_count(G_INT64_DT)
+                        if G_USE_MULTIPLE_RECV:
+                            bufferBase = index*recv_size
+                        else:
+                            bufferBase = 0
+
+                        for j in range(recvUpdates):
+                            inmsg = N.array(LocalRecvBuffer[bufferBase+j], type=N.UInt64)
+                            LocalOffset = (inmsg & (TableSize - 1)) - GlobalStartMyProc
+                            Table[LocalOffset] ^= inmsg
+
+                    elif status.MPI_TAG == G_FINISHED_TAG: # we got a done message.  Thanks for playing...
+                        NumberReceiving -= 1
+                    else:
+                        self.comm.Abort()
+
+                    if G_USE_MULTIPLE_RECV:
+                        self.comm.Irecv(LocalRecvBuffer[index*recv_size:(index+1)*recv_size], mpi.ANY_SOURCE, mpi.ANY_TAG, inreq[index])
+                    else:
+                        self.comm.Irecv(LocalRecvBuffer, mpi.ANY_SOURCE, mpi.ANY_TAG, inreq)
+
+                if have_done and NumberReceiving > 0:
+                    continue
+                break
+
+            have_done = outreq.Test(mpi.STATUS_IGNORE)
+            if have_done:
+                outreq[:] = mpi.REQUEST_NULL
+                pe, peUpdates = buckets.GetUpdates(LocalSendBuffer)
+                self.comm.Isend(LocalSendBuffer[:peUpdates], pe, G_UPDATE_TAG, outreq)
+                pendingUpdates -= peUpdates
+
+        for proc_count in range(NumProcs): # send our done messages
+            if proc_count == MyProc:
+                finish_req[MyProc] = mpi.REQUEST_NULL
+                continue
+
+            # send garbage - who cares, no one will look at it
+            self.comm.Isend(Ran, proc_count, G_FINISHED_TAG, finish_req[proc_count])
+
+        while NumberReceiving > 0: # Finish everyone else up...
+            if G_USE_MULTIPLE_RECV:
+                index = inreq[:NumRecvs].Waitany(status)
+            else:
+                inreq.Wait(status)
+
+            if status.MPI_TAG == G_UPDATE_TAG:
+                recvUpdates = status.Get_count(G_INT64_DT)
+
+                if G_USE_MULTIPLE_RECV:
+                    bufferBase = index * recv_size
+                else:
+                    bufferBase = 0
+
+                for j in range(recvUpdates):
+                    inmsg = N.array(LocalRecvBuffer[bufferBase+j], type=N.UInt64)
+                    LocalOffset = (inmsg & (TableSize - 1)) - GlobalStartMyProc
+                    Table[LocalOffset] ^= inmsg
+
+            elif status.MPI_TAG == G_FINISHED_TAG: # we got a done message.  Thanks for playing...
+                NumberReceiving -= 1
+            else:
+                self.comm.Abort()
+
+            if G_USE_MULTIPLE_RECV:
+                self.comm.Irecv(LocalRecvBuffer[index*recv_size:(index+1)*recv_size], mpi.ANY_SOURCE, mpi.ANY_TAG, inreq[index])
+            else:
+                self.comm.Irecv(LocalRecvBuffer, mpi.ANY_SOURCE, mpi.ANY_TAG, inreq)
+
+        finish_req.Waitall(finish_statuses)
+
+        if G_USE_MULTIPLE_RECV:
+            for j in range(NumRecvs):
+                inreq[j].Cancel()
+                inreq[j].Wait(mpi.STATUS_IGNORE)
+        else:
+            inreq.Cancel()
+            inreq.Wait(mpi.STATUS_IGNORE)
+
+        return Table
+
+    def mpira_verify(self, logTableSize, TableSize, LocalTableSize, MinLocalTableSize, GlobalStartMyProc, Top,
+                  Remainder, ProcNumUpdates, Table):
+        FALSE = 0
+        TRUE = 1
+        BUCKET_SIZE = 1024
+        DONE = 0
+
+        SLOT_CNT = 1
+        FIRST_SLOT = 2
+
+        NumProcs, MyProc = self.size, self.rank
+
+        LocalAllDone = FALSE
+
+        LocalBuckets =  N.array(shape=NumProcs*(BUCKET_SIZE+FIRST_SLOT), type=N.UInt64)
+        GlobalBuckets = N.array(shape=NumProcs*(BUCKET_SIZE+FIRST_SLOT), type=N.UInt64)
+
+        SendCnt = ProcNumUpdates
+        Ran = N.array([starts(4 * GlobalStartMyProc)], type=N.UInt64)
+
+        PeCheckDone = N.array(shape=NumProcs, type=N.Int64)
+        PeCheckDone[:] = FALSE
+
+        while LocalAllDone == FALSE:
+            if SendCnt > 0:
+                for i in range(NumProcs): # Initalize local buckets
+                    PeBucketBase = i * (BUCKET_SIZE+FIRST_SLOT)
+                    LocalBuckets[PeBucketBase+SLOT_CNT] = FIRST_SLOT
+                    LocalBuckets[PeBucketBase+DONE] = FALSE
+
+                # Fill local buckets until one is full or out of data
+                NextSlot = FIRST_SLOT
+                while NextSlot != (BUCKET_SIZE+FIRST_SLOT) and SendCnt>0:
+                    Ran = (Ran << 1) ^ ((Ran.astype(N.Int64) < 0)[0] and G_POLY or 0)
+                    GlobalOffset = (Ran & (TableSize-1))[0]
+                    if GlobalOffset < Top:
+                        WhichPe = GlobalOffset / (MinLocalTableSize + 1)
+                    else:
+                        WhichPe = (GlobalOffset - Remainder) / MinLocalTableSize
+                    PeBucketBase = WhichPe * (BUCKET_SIZE+FIRST_SLOT)
+                    NextSlot = LocalBuckets[PeBucketBase+SLOT_CNT]
+                    LocalBuckets[PeBucketBase+NextSlot] = Ran[0]
+                    NextSlot += 1
+                    LocalBuckets[PeBucketBase+SLOT_CNT] = NextSlot
+                    SendCnt -= 1
+
+                if SendCnt == 0:
+                    LocalBuckets[DONE:DONE+NumProcs*(BUCKET_SIZE+FIRST_SLOT):BUCKET_SIZE+FIRST_SLOT] = TRUE
+
+            # End of sending loop
+
+            self.comm.Barrier()
+
+            LocalAllDone = TRUE
+
+            # Now move all the buckets to the appropriate pe
+            self.comm.Alltoall(LocalBuckets[:BUCKET_SIZE+FIRST_SLOT], GlobalBuckets[:BUCKET_SIZE+FIRST_SLOT])
+
+            for i in range(NumProcs):
+                if PeCheckDone[i] == FALSE:
+                    PeBucketBase = i * (BUCKET_SIZE+FIRST_SLOT)
+                    PeCheckDone[i] = GlobalBuckets[PeBucketBase+DONE]
+                    for j in range (FIRST_SLOT, GlobalBuckets[PeBucketBase+SLOT_CNT]):
+                        RanTmp = N.array([GlobalBuckets[PeBucketBase+j]], type=N.UInt64)
+                        GlobalOffset = RanTmp & (TableSize - 1)
+                        LocalOffset = GlobalOffset - GlobalStartMyProc
+                        Table[LocalOffset] ^= RanTmp
+                    LocalAllDone &= PeCheckDone[i]
+        # no more local data
+
+        # FIXME: make it faster
+        errors = 0
+        for i in range(LocalTableSize):
+            if Table[i] != i + GlobalStartMyProc:
+                errors += 1
+
+        return errors
 
     def mpira(self):
-        total_mem = self.HPL_max_proc_mem * self.size / SIZEOF_UINT64
+        total_mem = self.HPL_max_proc_mem * self.size / G_SIZEOF_UINT64
 
         TableSize = 1
         logTableSize = 0
@@ -125,6 +462,8 @@ class HPCC:
             total_mem /= 2
             logTableSize += 1
             TableSize <<= 1
+
+        NumProcs, MyProc = self.size, self.rank
 
         MinLocalTableSize = TableSize / NumProcs # Minimum local table size - some processors have an additional entry
         Remainder = TableSize  - MinLocalTableSize * NumProcs # Number of processors with (LocalTableSize + 1) entries
@@ -138,24 +477,34 @@ class HPCC:
             LocalTableSize = MinLocalTableSize
             GlobalStartMyProc = (MinLocalTableSize * MyProc) + Remainder
 
-        self.Table = N.array(type=N.UInt64, shape=LocalTableSize)
-
         NumUpdates_Default = 4 * TableSize # Default number of global updates to table: 4x number of table entries
 
         ProcNumUpdates = 4*LocalTableSize
         NumUpdates = NumUpdates_Default
 
-        mpi.Barrier(self.comm)
+        self.comm.Barrier()
 
         t = -mpi.Wtime()
 
-        self.mpira_any(logTableSize, TableSize, LocalTableSize, MinLocalTableSize, GlobalStartMyProc, Top,
+        Table = self.mpira_any(logTableSize, TableSize, LocalTableSize, MinLocalTableSize, GlobalStartMyProc, Top,
                        Remainder, ProcNumUpdates)
 
-        mpi.Barrier(self.comm)
+        self.comm.Barrier()
+
         t += mpi.Wtime()
 
-        del self.Table
+        self.results["mpira_gups"] = 1e-9 * ProcNumUpdates / t
+        self.results["mpira_updates"] = ProcNumUpdates
+        self.results["mpira_time"] = t
+
+        t = -mpi.Wtime()
+        errors = self.mpira_verify(logTableSize, TableSize, LocalTableSize, MinLocalTableSize, GlobalStartMyProc,
+        #errors = mpi.verify(logTableSize, TableSize, LocalTableSize, MinLocalTableSize, GlobalStartMyProc,
+                          Top, Remainder, ProcNumUpdates, Table)
+        t += mpi.Wtime()
+
+        self.results["mpira_vtime"] = t
+        self.results["mpira_errors"] = errors
 
 def main(argv):
     mpi.Init(argv)
@@ -163,6 +512,7 @@ def main(argv):
     hpcc = HPCC("hpccinf.txt", "hpccoutf.txt")
 
     hpcc.ep_stream()
+    hpcc.mpira()
 
     keys = hpcc.results.keys()
     keys.sort()
--- python/mpi.pyx
+++ python/mpi.pyx
@@ -1,4 +1,7 @@
 # -*- Pyrex -*-
+#
+# Pyrex functions/methods receive C-typed argments (other than primitive C types), only generic Python types or Pyrex C types (except of C-only functions defined with `cdef')
+#
 
 cdef extern from "numarray/libnumarray.h":
     ctypedef int maybelong
@@ -36,6 +39,10 @@ cdef extern from "mpi.h":
         pass
     ctypedef struct MPI_Op:
         pass
+    ctypedef struct MPI_Request:
+        pass
+    ctypedef struct MPI_Status:
+        int MPI_TAG
 
     MPI_Comm MPI_COMM_WORLD
     MPI_Datatype MPI_DOUBLE
@@ -43,21 +50,54 @@ cdef extern from "mpi.h":
     MPI_Datatype MPI_LONG
     MPI_Datatype MPI_LONG_LONG_INT
     MPI_Op MPI_SUM
+    MPI_Request MPI_REQUEST_NULL
+    MPI_Status *MPI_STATUS_IGNORE
+    MPI_Status *MPI_STATUSES_IGNORE
 
     int MPI_SUCCESS
+    int MPI_ANY_SOURCE
+    int MPI_ANY_TAG
     int MPI_Abort(MPI_Comm comm, int errcode)
     int MPI_Allreduce(void *sbuf, void *rbuf, int count, MPI_Datatype dtype, MPI_Op op, MPI_Comm comm)
-    int MPI_Init(int *argc, char ***argv)
-    int MPI_Finalize()
+    int MPI_Alltoall(void *sbuf, int scount, MPI_Datatype sdtype, void* rbuf, int rcount, MPI_Datatype rdtype, MPI_Comm comm)
+    int MPI_Barrier(MPI_Comm comm)
+    int MPI_Cancel(MPI_Request *preq)
     int MPI_Comm_rank(MPI_Comm comm, int *rank)
     int MPI_Comm_size(MPI_Comm comm, int *rank)
+    int MPI_Finalize()
+    int MPI_Get_count(MPI_Status *stat, MPI_Datatype dtype, int *count)
+    int MPI_Init(int *argc, char ***argv)
+    int MPI_Irecv(void *buf, int count, MPI_Datatype dtype, int src, int tag, MPI_Comm comm, MPI_Request *req)
+    int MPI_Isend(void *buf, int count, MPI_Datatype dtype, int dest, int tag, MPI_Comm comm, MPI_Request *req)
     int MPI_Send(void *buf, int count, MPI_Datatype dtype, int dest, int tag, MPI_Comm comm)
+    int MPI_Test(MPI_Request *req, int *flag, MPI_Status *stat)
+    int MPI_Testany(int count, MPI_Request *reqs, int *index, int *flag, MPI_Status *stat)
+    int MPI_Wait(MPI_Request *preq, MPI_Status *stat)
+    int MPI_Waitall(int count, MPI_Request *reqs, MPI_Status *stats)
+    int MPI_Waitany(int count, MPI_Request *reqs, int *index, MPI_Status *stat)
     double MPI_Wtime()
     double MPI_Wtick()
 
+cdef extern from "pyxutil.h":
+    cdef enum _Type:
+        _Request
+        _Status
+    void *pyxmpi_alloc(_Type t, int n) # allocator for MPI data types
+    int Cverify(int logTableSize, int TableSize, int LocalTableSize, int MinLocalTableSize, int GlobalStartMyProc, int Top, int Remainder, int ProcNumUpdates, void *Table) 
+
+def verify(int logTableSize, int TableSize, int LocalTableSize, int MinLocalTableSize, int GlobalStartMyProc, int Top, int Remainder, int ProcNumUpdates, _numarray Table):
+    return Cverify(logTableSize, TableSize, LocalTableSize, MinLocalTableSize, GlobalStartMyProc, Top, Remainder, ProcNumUpdates, Table.data)
+
 cdef MPI_Datatype GetTC(_numarray a, int *c):
     c[0] = a.dimensions[0]
-    return MPI_DOUBLE
+    typ = a.descr.type
+    #print c[0], typ, a.descr.elsize
+    if 105 == typ: # i (Int)
+        return MPI_INT
+    elif 100 == typ: # d (Float64)
+        return MPI_DOUBLE
+    elif typ in (78, 85): # U (UInt64), N (Int64)
+        return MPI_LONG_LONG_INT
 
 cdef class Op:
     cdef MPI_Op op
@@ -66,16 +106,150 @@ cdef class Op:
             self.op = MPI_SUM
 
 cdef class Datatype:
-    cdef MPI_Datatype tpe
+    cdef MPI_Datatype dtype
     def __init__(self, t):
         if "LONG" == t:
-            self.tpe = MPI_LONG
+            self.dtype = MPI_LONG
         else:
-            self.tpe = MPI_LONG_LONG_INT
+            self.dtype = MPI_LONG_LONG_INT
+
+cdef class Status:
+    cdef MPI_Status *status
+    cdef int n
+
+    def __init__(self, int n=1):
+        assert n > 0
+        self.status = <MPI_Status *>pyxmpi_alloc(_Status, n)
+        self.n = n
+
+    property MPI_TAG:
+        def __get__(self):
+            assert self.n == 1
+            return self.status[0].MPI_TAG
+
+    def Get_count(self, Datatype dtype):
+        cdef int count
+
+        assert self.n == 1
+
+        #FIXME: return value of MPI_Get_count
+        MPI_Get_count(self.status, dtype.dtype, &count)
+        return count
+
+cdef class Request:
+    cdef MPI_Request *req
+    cdef int n
+
+    def __init__(self, int n=1, int view=0, object base=None, int idx=0):
+        #print n, view, idx
+        cdef Request base_
+        if view:
+            base_ = base
+            self.req = base_.req + idx
+            self.n = n
+            return
+
+        assert n > 0
+        self.req = <MPI_Request *>pyxmpi_alloc(_Request, n)
+        self.n = n
+
+    def __getitem__(self, object x):
+        cdef int start, stop #, step
+        if type(x) is type(slice(None)):
+            start = 0
+            if x.start is not None:
+                start = x.start
+                assert 0 <= start < n
+            stop = self.n
+            if x.stop is not None:
+                stop = x.stop
+                assert start < stop <= self.n
+            #print x, start, stop
+            return Request(stop-start, 1, self, start)
+        elif type(x) is type(0):
+            return Request(1, 1, self, x)
+        return None
+        assert 0 <= i < self.n
+        cdef Request r
+
+        r = Request(i, 1, self)
+        #r.req[0] = self.req[i]
+        return r
+
+    def __setitem__(self, object x, object y):
+        cdef int i
+        assert y is None
+        #print x, y, type(x)
+        if type(x) is type(slice(None)):
+            for i from 0 <= i < self.n:
+                self.req[i] = MPI_REQUEST_NULL
+        elif type(x) is type(0):
+            i = x
+            self.req[i] = MPI_REQUEST_NULL
+
+    def Cancel(self):
+        assert self.n == 1
+        MPI_Cancel(self.req)
+
+    def Testany(self, Status status):
+        cdef int index, flag
+        cdef MPI_Status *lstat
+
+        lstat = MPI_STATUS_IGNORE
+        if status is not None:
+            lstat = status.status
+
+        #FIXME: return value of MPI_Testany() ?
+        MPI_Testany(self.n, self.req, &index, &flag, lstat)
+        return index, flag
+
+    def Test(self, Status status):
+        cdef int flag
+        cdef MPI_Status *lstat
+
+        lstat = MPI_STATUS_IGNORE
+        if status is not None:
+            lstat = status.status
+
+        #FIXME: return value of MPI_Test() ?
+        MPI_Test(self.req, &flag, lstat)
+        return flag
+
+    def Wait(self, Status status):
+        cdef MPI_Status *lstat
+
+        assert self.n == 1
+
+        lstat = MPI_STATUSES_IGNORE
+        if status is not None:
+            lstat = status.status
+
+        return MPI_Wait(self.req, lstat)
+
+    def Waitall(self, Status status):
+        cdef MPI_Status *lstat
+
+        lstat = MPI_STATUSES_IGNORE
+        if status is not None:
+            lstat = status.status
+
+        return MPI_Waitall(self.n, self.req, lstat)
+
+    def Waitany(self, Status status):
+        cdef int index
+        cdef MPI_Status *lstat
+
+        lstat = MPI_STATUS_IGNORE
+        if status is not None:
+            lstat = status.status
+
+        #FIXME: return value of MPI_Waitany() ?
+        MPI_Waitany(self.n, self.req, &index, lstat)
+        return index
 
 cdef class Comm:
     cdef MPI_Comm comm
-    def __new__(self):
+    def _set_world(self):
         self.comm = MPI_COMM_WORLD
 
     def rank(self):
@@ -93,19 +267,57 @@ cdef class Comm:
         rv = MPI_Abort(self.comm, errcode)
         return rv
 
+    def Alltoall(self, _numarray sbuf, _numarray rbuf):
+        cdef int scount, rcount
+        cdef MPI_Datatype sdtype, rdtype
+
+        sdtype = GetTC(sbuf, &scount)
+        rdtype = GetTC(rbuf, &rcount)
+
+        return MPI_Alltoall(sbuf.data, scount, sdtype, rbuf.data, rcount, rdtype, self.comm)
+
     def Allreduce(self, _numarray src, _numarray dst, Op op):
         cdef int count
-        cdef MPI_Datatype typ
+        cdef MPI_Datatype dtype
 
-        typ = GetTC(src, &count)
-        return MPI_Allreduce(src.data, dst.data, count, typ, op.op, self.comm)
+        dtype = GetTC(src, &count)
+        return MPI_Allreduce(src.data, dst.data, count, dtype, op.op, self.comm)
+
+    def Barrier(self): MPI_Barrier(self.comm)
+
+    def Irecv(self, _numarray buf, int src, int tag, Request req):
+        cdef int count
+        cdef MPI_Datatype dtype
+
+        assert req.n == 1
+
+        dtype = GetTC(buf, &count)
+        return MPI_Irecv(buf.data, count, dtype, src, tag, self.comm, req.req)
+
+    def Isend(self, _numarray buf, int dest, int tag, Request req):
+        cdef int count
+        cdef MPI_Datatype dtype
+
+        assert req.n == 1
+
+        dtype = GetTC(buf, &count)
+        return MPI_Isend(buf.data, count, dtype, dest, tag, self.comm, req.req)
 
     def Send(self, _numarray a, int dest, int tag):
         cdef int count
-        cdef MPI_Datatype typ
+        cdef MPI_Datatype dtype
 
-        typ = GetTC(a, &count)
-        return MPI_Send(a.data, count, typ, dest, tag, self.comm)
+        dtype = GetTC(a, &count)
+        return MPI_Send(a.data, count, dtype, dest, tag, self.comm)
+
+def data_type(_numarray a):
+    cdef Datatype dtype
+    cdef int count
+
+    dtype = Datatype("")
+    dtype.dtype = GetTC(a, &count)
+
+    return dtype
 
 def Init(argv):
     cdef char *cargv[101]
@@ -140,7 +352,7 @@ def Comm_size(Comm comm):
 def Wtime():
     return MPI_Wtime()
 
-def Wtick():
+cdef Wtick():
     return MPI_Wtick()
 
 global SUM
@@ -154,3 +366,16 @@ LONG_LONG_INT = Datatype("LONG_LONG_INT")
 
 global COMM_WORLD
 COMM_WORLD = Comm()
+COMM_WORLD._set_world()
+
+global ANY_SOURCE
+ANY_SOURCE = MPI_ANY_SOURCE
+
+global ANY_TAG
+ANY_TAG = MPI_ANY_TAG
+
+global STATUS_IGNORE
+STATUS_IGNORE = None
+
+global REQUEST_NULL
+REQUEST_NULL = None
