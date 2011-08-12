d482f34bae83e992dcc48017969e0e1f5442dcb0
Piotr Luszczek <luszczek@icl.utk.edu>
2011-08-12 14:39:43 -0400

    Initial version of memory checker FFT.

---
+++ work/mem4fft.cc
@@ -0,0 +1,135 @@
+/* -*- mode: C; tab-width: 2; indent-tabs-mode: nil; fill-column: 79; coding: iso-latin-1-unix -*- */
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "mem4fft.h"
+
+impl_complex_t G_complex_val;
+char G_anon[10] = "ANONYMOUS";
+
+impl_complex_t&
+fftw_complex::index_access(int idx) {
+  *(this->last_idx) = this->offset + idx;
+  if (*(this->largest_idx) < idx)
+    *(this->largest_idx) = this->offset + idx;
+
+  return G_complex_val;
+}
+
+impl_complex_t& fftw_complex::operator [](long unsigned int idx) {
+  return this->index_access(idx);
+}
+
+impl_complex_t& fftw_complex::arr2d(int idx) {
+  return this->index_access(idx);
+}
+
+impl_complex_t& fftw_complex::arr3d(int idx) {
+  return this->index_access(idx);
+}
+
+fftw_complex *fftw_complex::ptr2d(int idx) {
+  this->index_access(idx);
+  return this;
+}
+
+impl_complex_t& fftw_complex::sqbracket(int idx) {
+  return this->index_access(idx);
+}
+
+fftw_complex::fftw_complex(fftw_complex& other) {
+  this->largest_idx = other.largest_idx;
+  this->offset = other.offset;
+  this->last_idx = other.last_idx;
+  this->name = other.name;
+  this->size = other.size;
+}
+
+fftw_complex::fftw_complex(const char *name, int size) {
+  this->largest_idx = &(this->largest_idx_storage);
+  this->last_idx = &(this->last_idx_storage);
+  *(this->largest_idx) = 0;
+  this->name = name;
+  this->size = size;
+  this->offset = 0;
+}
+
+fftw_complex::fftw_complex(void) {
+  this->largest_idx = &(this->largest_idx_storage);
+  this->last_idx = &(this->last_idx_storage);
+  this->name = G_anon;
+  this->size = size;
+  this->offset = 0;
+}
+
+void c_assgn(fftw_complex& d, fftw_complex&s) { }
+
+void c_assgn(impl_complex_t& d, fftw_complex&s) { }
+
+void c_assgn(impl_complex_t& d, impl_complex_t&s) { }
+
+void c_assgn(fftw_complex& d, impl_complex_t&s) { }
+
+void c_mul3v(fftw_complex& v1, fftw_complex& v2, fftw_complex& v3) {}
+
+void
+fftw_complex::show_stat(void) {
+  printf( "%s[%d] %d\n", this->name, this->size, *(this->largest_idx) );
+}
+
+int
+plan_init(hpcc_fftw_plan p, int n) {
+  p->n = n;
+  p->w1 = new fftw_complex("w1", n);
+  p->w2 = new fftw_complex("w2", n);
+  p->ww = new fftw_complex("ww", n);
+  p->ww2 = new fftw_complex("ww2", n);
+  p->ww3 = new fftw_complex("ww3", n);
+  p->ww4 = new fftw_complex("ww4", n);
+  p->c = new fftw_complex("c", n);
+  p->d = new fftw_complex("d", n);
+
+  c_re( ARR2D( p->w1, 0, 0, 0 ) ) = 0.0;
+
+  return 0;
+}
+
+void
+show_single_stat(fftw_complex *fc) {
+  printf( "", fc);
+}
+
+int
+plan_show_stats(hpcc_fftw_plan p) {
+  p->w1->show_stat();
+  p->w2->show_stat();
+  p->ww->show_stat();
+  p->ww2->show_stat();
+  p->ww3->show_stat();
+  p->ww4->show_stat();
+  p->c->show_stat();
+  p->d->show_stat();
+}
+
+int
+main(int argc, char *argv[]) {
+  struct hpcc_fftw_plan_s ps;
+  int n = 1024;
+  fftw_complex *in, *out;
+
+  in = new fftw_complex("INPUT", n);
+  out = new fftw_complex("INPUT", n);
+
+  plan_init( &ps, n );
+
+  HPCC_zfft1d( n, in, out, 0, &ps );
+
+  in->show_stat();
+  out->show_stat();
+
+  plan_show_stats( &ps );
+
+  return 0;
+}
---
+++ work/mem4fft.h
@@ -0,0 +1,65 @@
+
+#define FFTE_NP 8
+#define FFTE_NBLK 16
+#define FFTE_L2SIZE 1048576
+#define FFTE_NDA2 65536
+
+#define ARR2D(a, i, j, lda) (a)->arr2d((i)+(j)*(lda))
+#define PTR2D(a, i, j, lda) (a)->ptr2d((i)+(j)*(lda))
+#define ARR3D(a, i, j, k, lda1, lda2) (a)->arr3d((i)+(lda1)*((j)+(k)*(lda2)))
+#define PTR3D(a, i, j, k, lda1, lda2) (a+(i)+(lda1)*((j)+(k)*(lda2)))
+#define ARR4D(a, i, j, k, l, lda1, lda2, lda3) a[(i)+(lda1)*((j)+(lda2)*((k)+(lda3)*(l)))]
+#define V3MIN(r, e, v) r = (e); V2MIN(r, v)
+#define V2MIN(r, v) r = (v) < r ? (v) : r
+#define EMAX(d, v, e) d=(e); d=d>(v)?d:(v)
+
+#define c_re(c)  ((c).real)
+#define c_im(c)  ((c).imag)
+
+typedef long unsigned int s64Int_t;
+
+typedef struct impl_complex_s {
+  double real, imag;
+} impl_complex_t;
+
+class fftw_complex {
+  impl_complex_t& index_access(int idx);
+
+public:
+  int size, offset;
+  int largest_idx_storage, last_idx_storage;
+  /* making these pointers creates an easy to spot (out of bounds SEGFAULT) error detection */
+  int *largest_idx, *last_idx;
+  const char *name;
+
+  fftw_complex(fftw_complex& other);
+  fftw_complex(const char *name, int n);
+  fftw_complex(void);
+  fftw_complex(fftw_complex *other, int offset);
+
+  impl_complex_t& operator [](long unsigned int idx);
+
+  impl_complex_t& arr2d(int idx);
+  impl_complex_t& arr3d(int idx);
+  fftw_complex *ptr2d(int idx);
+  impl_complex_t& sqbracket(int idx);
+
+  void show_stat();
+};
+
+void c_assgn(fftw_complex& d, fftw_complex& s);
+void c_assgn(impl_complex_t& d, impl_complex_t& s);
+void c_assgn(fftw_complex& d, impl_complex_t& s);
+void c_assgn(impl_complex_t& d, fftw_complex& s);
+void c_mul3v(fftw_complex& v1, fftw_complex& v2, fftw_complex& v3);
+
+typedef struct hpcc_fftw_plan_s {
+  int n;
+  fftw_complex *w1, *w2, *ww, *ww2, *ww3, *ww4, *c, *d;
+} *hpcc_fftw_plan;
+
+extern int HPCC_fft235(fftw_complex *a, fftw_complex *b, fftw_complex *w, int n, const int *ip);
+
+extern int HPCC_settbl(fftw_complex *w, int n);
+extern int HPCC_factor235(int n, int *ip);
+extern int HPCC_zfft1d(int n, fftw_complex *a, fftw_complex *b, int iopt, hpcc_fftw_plan p);
