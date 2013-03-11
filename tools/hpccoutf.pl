a123fd020d1956999178ff0cbbc036b087821459
Piotr Luszczek <luszczek@icl.utk.edu>
2013-03-11 09:05:49 -0400

    Better format for output and fix for units.

--- tools/hpccoutf.pl
+++ tools/hpccoutf.pl
@@ -1,15 +1,30 @@
 #! /usr/bin/perl
 
 #
-# Usage: hpccoutf.pl < hpccoputf.txt
+# Usage: hpccoutf.pl -a < hpccoputf.txt To show all parameters
+#        hpccoutf.pl -w < hpccoputf.txt To show web parameters
 #
 
 use strict;
-use diagnostics;
+#use diagnostics;
+use Getopt::Std;
+$Getopt::Std::STANDARD_HELP_VERSION  = 1;
+our ( $opt_a, $opt_w, $opt_f, $value, $count, $key ) = 0;
+getopts("awf:");
 
-$/       = undef;
-my $file = <STDIN>;
-my %hpccout = $file =~ /^(\w+)=(\d.*)$/mg;
+unless ( $opt_a || $opt_w && $opt_f ) {
+    print "\n";
+    print "Usage: $0 -a -f hpccoutf.txt For all parameters\n";
+    print "       $0 -w -f hpccoutf.txt For web parameters\n";
+    exit;
+}
+
+$/           = undef;
+open HPPCOUTF, $opt_f or die $!;
+my $file     = <HPPCOUTF>;
+my %hpccoutf = $file =~ /^(\w+)=(\d.*)$/mg;
+close HPPCOUTF;
+
 
 my @walkorder = (
   'HPL_Tflops',                      'PTRANS_GBs',
@@ -17,50 +32,64 @@ my @walkorder = (
   'StarSTREAM_Triad*CommWorldProcs', 'StarSTREAM_Triad',
   'StarDGEMM_Gflops',                'RandomlyOrderedRingBandwidth_GBytes',
   'RandomlyOrderedRingLatency_usec' );
+
 my @walkunits = (
   'Tera Flops per Second',   'Tera Bytes per Second',
   'Giga Updates per Second', 'Tera Flops per Second',
   'Tera Bytes per Second',   'Giga Bytes per Second',
   'Giga Flops per Second',   'Giga Bytes per second',
   'micro-seconds');
+
 my %crosswalk = (
-    HPL_Tflops           => 'G-HPL',
-    PTRANS_GBs           => 'G-PTRANS',
-    MPIRandomAccess_GUPs =>  'G-RandomAccess',
-    MPIFFT_Gflops        =>  'G-FFT',
-    "StarSTREAM_Triad*CommWorldProcs"   =>  'EP-STREAM Sys',
-    CommWorldProcs                      =>  'MPI Processes',
-    # StarSTREAM_Triad * CommWorldProcs =>   EP-STREAM Sys
-    StarSTREAM_Triad                    =>  'EP-STREAM Triad',
-    StarDGEMM_Gflops                    =>  'EP-DGEMM',
-    RandomlyOrderedRingBandwidth_GBytes =>  'RandomRing Bandwidth',
-    RandomlyOrderedRingLatency_usec     =>  'RandomRing Latency'
+    HPL_Tflops                          => 'G-HPL',
+    PTRANS_GBs                          => 'G-PTRANS',
+    MPIRandomAccess_GUPs                => 'G-RandomAccess',
+    MPIFFT_Gflops                       => 'G-FFT',
+    'StarSTREAM_Triad*CommWorldProcs'   => 'EP-STREAM Sys',
+    CommWorldProcs                      => 'MPI Processes',
+    StarSTREAM_Triad                    => 'EP-STREAM Triad',
+    StarDGEMM_Gflops                    => 'EP-DGEMM',
+    RandomlyOrderedRingBandwidth_GBytes => 'RandomRing Bandwidth',
+    RandomlyOrderedRingLatency_usec     => 'RandomRing Latency'
 );
 
-my $show_all = 1;
-my $show_web = 0;
+if ( $opt_a ) { show_all(); }
+if ( $opt_w ) { show_web(); }
 
-if ( $show_all ) {
-    show_all();
-}
-if ( $show_web ) {
-    show_web();
-}
-
+###  Show all parameters from hpcc output file 
 sub show_all {
-    foreach my $key ( sort keys %hpccout ) {
-        print $key . ': ' . $hpccout{"$key"} . "\n";
+    foreach $key ( sort keys %hpccoutf ) {
+        print $key . ': ' . $hpccoutf{"$key"} . "\n";
     }
 }
 
+###  Show web parameters from hpcc output file
 sub show_web {
-    my $count = 0;
-    foreach my $key ( @walkorder ) {
+    print "\n";
+    foreach $key ( @walkorder ) {
+        $value = '';
         if ( $key eq 'StarSTREAM_Triad*CommWorldProcs') {
-            print "$key ($crosswalk{$key}) " . $hpccout{'StarSTREAM_Triad'} * $hpccout{'CommWorldProcs'} . " $walkunits[$count]\n";
+            $value = $hpccoutf{'StarSTREAM_Triad'} * $hpccoutf{'CommWorldProcs'} / 1000;
+            write;
+        } elsif ( $key eq 'PTRANS_GBs' or $key eq 'MPIFFT_Gflops' ) {
+            $value = $hpccoutf{$key} / 10000;
+            write;
         } else {
-            print "$key ($crosswalk{$key}) $hpccout{$key} $walkunits[$count]\n";
+            $value = $hpccoutf{$key};
+            write;
         }
         $count++;
     }
+    print '--------------------------------------------------------------------------------------------------'."\n";
 }
+
+format STDOUT_TOP =
+--------------------------------------------------------------------------------------------------
+HPCCOUTF NAME                        WEB NAME                      VALUE   UNITS
+--------------------------------------------------------------------------------------------------
+.
+
+format STDOUT =
+@<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<  @<<<<<<<<<<<<<<<<<<<  @#######.####   @<<<<<<<<<<<<<<<<<<<<<<
+$key,                                $crosswalk{$key},     $value,         $walkunits[$count]
+.
