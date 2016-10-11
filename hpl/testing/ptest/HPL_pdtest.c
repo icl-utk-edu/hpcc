bc5525d4ab441492cfc6b5b21bc472aed1ba25a4
Piotr Luszczek <luszczek@icl.utk.edu>
2016-10-11 16:47:06 -0400

    Add deallocation on processes that allocated memory succesfully

--- hpl/testing/ptest/HPL_pdtest.c
+++ hpl/testing/ptest/HPL_pdtest.c
@@ -178,6 +178,8 @@ void HPL_pdtest
                     "[%d,%d] %s", info[1], info[2],
                     "Memory allocation failed for A, x and b. Skip." );
       (TEST->kskip)++;
+      /* some processes might have succeeded with allocation */
+      if (vptr) free(vptr);
       return;
    }
 /*
